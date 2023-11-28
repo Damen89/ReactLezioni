e1a26a7434cc836b69bd8864464efa8f2cb3dc38
Damen89 <dario.mennillo@gmail.com>
2023-11-28 17:16:44 +0100

    up

--- Lesson2/LEGGIMI.md
+++ Lesson2/LEGGIMI.md
@@ -1,3 +1,31 @@
 # REUSABLE Components with Props
 React ci permette di passare dati attraverso un principio chiamato Prop
 ![](schermate/schermata4.png?raw=true)
+
+## Cosa sono i Props
+Sono degli attributi custom che vado a definire nel momento in cui vado a renderizzare un component attraverso il suo nome.
+```
+ <ul>
+    <CoreConcept titolo="Components" descrizione="Cosa sono e come si usano i components" />
+    <CoreConcept/>
+    <CoreConcept/>
+    <CoreConcept/>
+</ul>
+```
+Gli attributi "titolo" e "descrizione" sono i Props. Utilizzati per poter "iniettare" dei valori diversi ogni volta che richiamo quelle proprietà.
+Ovviamente non sono limitato alle string ma posso passare qualsiasi tipo di dato in quei props se serve.
+
+I props a loro volta vanno definiti all'interno della funzione che genera il component. Vanno trattati comme oggetti.
+
+![](schermate/schermata5.png?raw=true)
+
+## Utilizzo alternativo dei Props
+Utilizzando il file data.js posso richiamare quelle proprietà che esistono all'esterno.
+Dato che in data.js ho `export CORE_CONCEPTS` nel momento in cui li devo importare nel file che li utilizza devo utilizzare le greffe `import {CORE_CONCEPTS} from "..."`.
+
+ATT: Quando mi trovo nella situazione in cui i props hanno lo stesso nome delle proprietà degli oggetti posso utilizzare l'operatore spread di JS  [...] con il quale è possibile mappare in un colpo solo tutte le proprietà.
+
+ATT2: A questo punto posso anche utilizzare direttamente la destrutturazione delle variabili nella funzione che crea il component andando a passare delle variabili che portano lo stesso nome delle proprietà degli oggetti. 
+
+(Restructuring Assignment)[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment#rest_property]
+
--- Lesson2/src/App.jsx
+++ Lesson2/src/App.jsx
@@ -1,45 +1,103 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+import { CORE_CONCEPTS } from "./data.js";
+
 const parole = ["Cruciali", "Fondamentali", "Importantissimi"];
 
-function getRandomNumber(){
-  return Math.floor(Math.random() * parole.length)
+function getRandomNumber() {
+  return Math.floor(Math.random() * parole.length);
 }
 
 function Header() {
-
   const parole2 = parole[getRandomNumber()]; //questa opzione è la migliore
 
-  return(
-  <header>
+  return (
+    <header>
+      <h1>React Essential</h1>
 
-    <h1>React Essential</h1>
+      <p>Concetti {parole[getRandomNumber()]} per una buona cosa</p>
+      <p>
+        Posso richiamare quelle definite in alto: {parole2} nella function
+        Header{" "}
+      </p>
 
-    <p>Concetti {parole[getRandomNumber()]} per una buona cosa</p>
-    <p>Posso richiamare quelle definite in alto: {parole2} nella function Header </p>
+      <p>Posso anche mostrare dei valori al volo {1 + 1}. </p>
+      <p>
+        Non posso inserire if-statement o for-loops all&apos; interno delle
+        graffe.{" "}
+      </p>
 
-
-    <p>Posso anche mostrare dei valori al volo {1+1}. </p>
-    <p>Non posso inserire if-statement o for-loops all&apos; interno delle graffe. </p>
-
-    {/* Anche l'src delle immagini è meglio andarselo a prendere dall'import in alto */}
-    <img src={reactLogo} alt="logo di React" />
-  </header>
+      {/* Anche l'src delle immagini è meglio andarselo a prendere dall'import in alto */}
+      <img src={reactLogo} alt="logo di React" />
+    </header>
   );
+}
+
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt="" />
+//       <h3>{props.title}</h3>
+//       <h4>{props.description}</h4>
+//     </li>
+//   );
+// }
+
+//Utilizzando questa feature di JS devo stare solo attendo al nome che do alle cose. Le quali devono corrispondere con le proprietà degli oggetti che sto "mappando".
+
+function CoreConcept({title, image, description}){
+  return(
+    <li>
+      <img src={image} alt="" />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
 }
 
 function App() {
   return (
     <div>
-      {/* Proprio come se fossimo in JS una volta dichiarata la function la devo utilizzare. La cosa bella di JSX è che quando ho un component allora lo posso semplicemente richiamare dentro il mio html usando il nome della function e quindi del component */}
-
-      {/* <Header></Header> */}
-
-    {/* oppure anche così con il self closing tag */}
-    <Header />
+      <Header />
       <main>
-        <h2>Cominciamo !!!</h2>
+        <section id="core-concepts">
+          <h2>Cominciamo !!!</h2>
+
+          {/* <ul>
+            <CoreConcept
+              title="Components"
+              description="cosa sono e cosa fanno i components"
+              image={reactLogo}
+            />
+
+            <CoreConcept
+              title="Props"
+              description="cosa sono e cosa fanno i components"
+              image={reactLogo}
+            /> 
+          </ul> */}
+
+          {/* Utilizzo i Core conceps che sono nel file data */}
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+
+          {/* Utilizzo dell'operatore Spread che mi facilita la vita */}
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+
+          </ul>
+        </section>
       </main>
     </div>
   );
